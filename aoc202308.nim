b6f0ac3d8a49915ec0a3f16b8a8ca2b63214a24f
Johannes Martinsson <johannes@antiklimax.se>
2023-12-09 01:07:53 +0100

    Simplify the codes for day 8

--- aoc202308.nim
+++ aoc202308.nim
@@ -1,5 +1,5 @@
 import aocd
-import std/[strutils, math, strscans, tables, intsets, sequtils, unittest]
+import std/[strutils, math, strscans, tables, sequtils, unittest]
 
 const example1 = """
 RL
@@ -35,121 +35,63 @@ XXX = (XXX, XXX)
 """
 
 type
-  Address = int
-  Node = tuple[left: int, right: int]
-
-func encode(str: string): Address =
-  for c in str:
-    let v = c.ord - '0'.ord
-    result = (result shl 8) or v
-
-func decode(address: Address): char =
-  let x: range[0..255] = address and 0xff
-  result = chr(x + '0'.ord)
-
-func fullDecode(address: Address): string =
-  let a: range[0..255] = (address shr 0) and 0xff
-  let b: range[0..255] = (address shr 8) and 0xff
-  let c: range[0..255] = (address shr 16) and 0xff
-  [c, b, a].mapIt(chr(it + '0'.ord)).join
-
-check "AAA".encode.decode == 'A'
-check "AAZ".encode.decode == 'Z'
-check "12X".encode.decode == 'X'
-check "AAA".encode.fullDecode == "AAA"
-check "AAZ".encode.fullDecode == "AAZ"
-check "12X".encode.fullDecode == "12X"
-check "ZZZ".encode.fullDecode == "ZZZ"
-
-template finished(cursors: seq[Address]): bool =
-  cursors.allIt((it and 0xff) == 42)
-
-func isEnd(address: Address): bool =
-  (address and 0xff) == 42
-
-check isEnd("AAA".encode) == false
-check isEnd("ZZZ".encode) == true
-
-func isStart(address: Address): bool =
-  (address and 0xff) == 17
-
-check isStart("AAA".encode) == true
-check isStart("ZZZ".encode) == false
+  Address = string
+  Node = object
+    left: Address
+    right: Address
 
 day 8:
-  let xlines = input.strip.splitLines
-  let instructions = xlines[0]
+  let
+    inputLines = input.strip.splitLines
+    instructions = inputLines[0]
   var
-    cursors: seq[Address] = @[]
-    starts: seq[Address] = @[]
-    targets: seq[Address] = @[]
-    graph = initTable[Address, Node]()
-  for line in xlines[2..^1]:
+    graph: Table[Address, Node]
+
+  # Build up the graph
+  for line in inputLines[2..^1]:
     let (ok, a, l, r) = line.scanTuple("$+ = ($+, $+)$.")
     doAssert ok
-    graph[encode(a)] = (left: encode(l), right: encode(r))
-    if a.encode.isStart:
-      cursors.add encode(a)
-      starts.add encode(a)
-    if a.encode.isEnd:
-      targets.add encode(a)
+    graph[a] = Node(left: l, right: r)
 
   part 1:
-    let start = starts[0]
-    let target = targets[0]
-
-    var steps = 0
-    var ip = 0
-    var cursor = start
-    while cursor != target:
-      steps += 1
-      case instructions[ip]:
-        of 'L':
-          cursor = graph[cursor].left
-        of 'R':
-          cursor = graph[cursor].right
-        else:
-          doAssert false
-      ip = (ip + 1) mod instructions.len
-    steps
+    var cursor = "AAA"
+    result = 0
+    while cursor != "ZZZ":
+      case instructions[result mod instructions.len]:
+        of 'L': cursor = graph[cursor].left
+        of 'R': cursor = graph[cursor].right
+        else: doAssert false
+      inc result
 
   part 2:
     # Having looked at a graph of the data, it is clear that we have a "circle"
     # graph for each entry node. So thea idea is to figure out how many turns of
     # the circles are needed for them all to line up at the end.
 
-    # Find the unique cycle length from each start and instruction offset.
     var
-      cycles = initTable[Address, var IntSet]()
+      cycles: seq[int]
       cursor: Address
       steps: int
-      ip: int
-    for start in starts:
-      cycles[start] = initIntSet()
-    for start in starts:
-      cursor = start
-      for iStart in 0..instructions.high:
-        ip = iStart
-        steps = 0
-        while not cursor.isEnd:
-          case instructions[ip]:
-            of 'L':
-              cursor = graph[cursor].left
-            of 'R':
-              cursor = graph[cursor].right
-            else:
-              doAssert false
-          ip = (ip + 1) mod instructions.len
-          steps += 1
-        cycles[start].incl steps
-    for k in cycles.keys:
-      cycles[k].excl 0
 
-    echo cycles
-    var firstCycles: seq[int] = @[]
-    for k, v in cycles:
-      firstCycles.add v.items.toSeq[0]
-    firstCycles.lcm
+    for start in graph.keys:
+      if start[2] != 'A':
+        continue
+      cursor = start
+      for i in 0..instructions.high:
+        steps = 0
+        while cursor[2] != 'Z':
+          case instructions[(steps + i) mod instructions.len]:
+            of 'L': cursor = graph[cursor].left
+            of 'R': cursor = graph[cursor].right
+            else: doAssert false
+          inc steps
+        if steps != 0:
+          cycles.add steps
+
+    # > The least common multiple of more than two integers a, b, c, . . . ,
+    # > usually denoted by lcm(a, b, c, . . .), is defined as the smallest
+    # > positive integer that is divisible by each of a, b, c,
+    cycles.lcm
 
   verifyPart(1, 21389)
   verifyPart(2, 21083806112641)
