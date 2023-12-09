d38b3b65c54ae412718f4245a060a51e2c236190
Johannes Martinsson <johannes@antiklimax.se>
2023-12-09 14:35:14 +0100

    Solve day 9 part 2

--- aoc202309.nim
+++ aoc202309.nim
@@ -36,8 +36,29 @@ day 9:
       result += x
 
   part 2:
-    0
+    result = 0
+    for nums in numberLines:
+      var stack = @[nums]
+
+      block buildup:
+        var keepGoing = true
+        while keepGoing:
+          let line = stack[^1]
+          var diff: seq[int]
+          keepGoing = false
+          for i in 1..line.high:
+            diff.add(line[i] - line[i - 1])
+            keepGoing = true
+          if keepGoing:
+            stack.add diff
+
+      var x = 0
+      while stack.len > 0:
+        x = (stack.pop)[0] - x
+      echo x
+      result += x
 
   # 2658601764 is too high
   verifyPart(1, 1921197370)
-  verifyPart(2, 0)
+  # 20942 is too high
+  verifyPart(2, 1124)
