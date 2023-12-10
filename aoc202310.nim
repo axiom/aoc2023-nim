8b04d18c44eca0ee2d7eb6804e4677990e0c8aec
Johannes Martinsson <johannes@antiklimax.se>
2023-12-10 11:54:01 +0100

    Clean up day 10 a bit

--- aoc202310.nim
+++ aoc202310.nim
@@ -1,203 +1,125 @@
 import aocd
-import std/[os, deques, strutils, math, sets, strscans, tables, sequtils, unittest]
-
-const example1 {.used.} = """
-.....
-.S-7.
-.|.|.
-.L-J.
-.....
-"""
-
-const example2 {.used.} = """
-..F7.
-.FJ|.
-SJ.L7
-|F--J
-LJ...
-"""
-
-const example3 {.used.} = """
-.F----7F7F7F7F-7....
-.|F--7||||||||FJ....
-.||.FJ||||||||L7....
-FJL7L7LJLJ||LJ.L-7..
-L--J.L7...LJS7F-7L7.
-....F-J..F7FJ|L7L7L7
-....L7.F7||L7|.L7L7|
-.....|FJLJ|FJ|F7|.LJ
-....FJL-7.||.||||...
-....L---J.LJ.LJLJ...
-"""
-
-const example4 {.used.} = """
-FF7FSF7F7F7F7F7F---7
-L|LJ||||||||||||F--J
-FL-7LJLJ||||||LJL-77
-F--JF--7||LJLJ7F7FJ-
-L---JF-JLJ.||-FJLJJ7
-|F|F-JF---7F7-L7L|7|
-|FFJF7L7F-JF7|JL---7
-7-L-JL7||F7|L7F-7F7|
-L.L7LFJ|||||FJL7||LJ
-L7JLJL-JLJLJL--JLJ.L
-"""
-
-const example5 {.used.} = """
-...........
-.S-------7.
-.|F-----7|.
-.||.....||.
-.||.....||.
-.|L-7.F-J|.
-.|..|.|..|.
-.L--J.L--J.
-...........
-"""
+import std/[strutils, sets, tables, sequtils, unittest]
 
 type
+  Grid = seq[string]
   Pos = object
     r, c: int
+  Dir = enum
+    North, South, East, West
 
-# Replace ascii pipe chars with unicode single box drawing characters.
+func charAt(g: Grid, p: Pos, default: char = '.'): char =
+  ## Get the character at a position, or '.' if out of bounds.
+  if p.r in g.low..g.high and p.c in g[0].low..g[0].high:
+    return g[p.r][p.c]
+  default
+
+func `+`(p: Pos, d: Dir): Pos =
+  ## Generate a new position by moving in a direction.
+  case d:
+    of North: Pos(r: p.r - 1, c: p.c)
+    of South: Pos(r: p.r + 1, c: p.c)
+    of East:  Pos(r: p.r,     c: p.c + 1)
+    of West:  Pos(r: p.r,     c: p.c - 1)
+
+const
+  Wards = {
+    North: "J|LS",
+    South: "7|FS",
+    West: "J-7S",
+    East: "F-LS",
+  }.toTable
+
+  Opposite = { North: South, South: North, East: West, West: East, }.toTable
+
+func opposite(d: Dir): Dir = Opposite[d]
+
+const fancyChars = {
+  'F': "╭", '7': "╮",
+  'J': "╯", 'L': "╰",
+  '|': "│", '-': "─",
+  '.': " ", 'S': "S",
+  'O': " ", 'I': "█"
+}.toTable
+
 proc fancy(c: char): string =
-  case c:
-    of '|': "║"
-    of '-': "═"
-    of 'F': "╔"
-    of '7': "╗"
-    of 'J': "╝"
-    of 'L': "╚"
-    else: $c
+  ## Replace input box symbols with fancier versions.
+  fancyChars.getOrDefault(c, $c)
 
 day 10:
+  # Parse and store the input
   let
     grid = input.strip.splitLines
-    width = grid[0].len
-    height = grid.len
-    start = block:
+    start = block findStart:
       var start = Pos(r: 0, c: 0)
-      for r in 0 ..< height:
-        for c in 0 ..< width:
+      for r in 0..grid.high:
+        for c in 0..grid[0].high:
           if grid[r][c] == 'S':
             start = Pos(r: r, c: c)
       start
 
-  part 1:
-    var steps = 0
-    var cur = start
-    var seen: HashSet[Pos]
-    seen.incl start
+  # Find the looped pipe in the grid
+  var
+    loop: HashSet[Pos]
+    curr = start
+    done = false
+  loop.incl start
+  while not done:
+    done = true # done if the loop dosn't find a new position
+    for dir in [North, South, East, West]:
+      let
+        nextPos = curr + dir
+        currFitsNext = grid.charAt(curr) in Wards[dir]
+        nextFitsCurr = grid.charAt(nextPos) in Wards[opposite dir]
 
-    while true:
-      var me = grid[cur.r][cur.c]
-      var north = if cur.r == 0: '.' else: grid[cur.r - 1][cur.c]
-      var south = if cur.r == grid.high: '.' else: grid[cur.r + 1][cur.c]
-      var east = if cur.c == grid[0].high: '.' else: grid[cur.r][cur.c + 1]
-      var west = if cur.c == 0: '.' else: grid[cur.r][cur.c - 1]
-
-      var northP = Pos(r: cur.r - 1, c: cur.c)
-      var southP = Pos(r: cur.r + 1, c: cur.c)
-      var eastP = Pos(r: cur.r, c: cur.c + 1)
-      var westP = Pos(r: cur.r, c: cur.c - 1)
-
-      inc steps
-
-      # Can I go north?
-      if   me in "J|LS" and north in "7|F" and northP notin seen:
-        cur = northP
-      elif me in "7|FS" and south in "J|L" and southP notin seen:
-        cur = southP
-      elif me in "F-LS" and east in "J-7" and eastP notin seen:
-        cur = eastP
-      elif me in "J-7S" and west in "F-L" and westP notin seen:
-        cur = westP
-      else:
+      if currFitsNext and nextFitsCurr and nextPos notin loop:
+        curr = nextPos
+        done = false
+        loop.incl curr
         break
 
-      seen.incl cur
-
-    steps div 2
+  part 1:
+    # Since we are dealing with a loop, the furthest we can go is half the loop.
+    loop.len div 2
 
   part 2:
-    var
-      cur = start
-      steps: int
-      path: seq[Pos]
-      loop: HashSet[Pos]
-
-    path.add cur
-    loop.incl start
-
-    while true:
-      var
-        me = grid[cur.r][cur.c]
-
-        north = if cur.r == 0: '.' else: grid[cur.r - 1][cur.c]
-        south = if cur.r == grid.high: '.' else: grid[cur.r + 1][cur.c]
-        east = if cur.c == grid[0].high: '.' else: grid[cur.r][cur.c + 1]
-        west = if cur.c == 0: '.' else: grid[cur.r][cur.c - 1]
-
-        northP = Pos(r: cur.r - 1, c: cur.c)
-        southP = Pos(r: cur.r + 1, c: cur.c)
-        eastP = Pos(r: cur.r, c: cur.c + 1)
-        westP = Pos(r: cur.r, c: cur.c - 1)
-
-      inc steps
-
-      # Can I go north?
-      if   me in "J|LS" and north in "7|F" and northP notin loop:
-        cur = northP
-      elif me in "7|FS" and south in "J|L" and southP notin loop:
-        cur = southP
-      elif me in "F-LS" and east in "J-7" and eastP notin loop:
-        cur = eastP
-      elif me in "J-7S" and west in "F-L" and westP notin loop:
-        cur = westP
-      else:
-        break
-
-      path.add cur
-      loop.incl cur
-
+    var cleanedGrid: seq[string]
     result = 0
-    for r in 0 ..< height:
+    for r in 0..grid.high:
+      cleanedGrid.add grid[r]
       var crossings = 0
-      var p = '.'
-      for c in 0 ..< width:
-        var g = grid[r][c]
-        let onPath = Pos(r: r, c: c) in path
+      var prevPipe = '.'
+      for c in 0..grid[r].high:
+        var currPipe = grid[r][c]
 
-        if onPath:
-          if   p == '.' and g == '|':
-            inc crossings
-          elif p == '|' and g == '|':
-            inc crossings
-          elif p == '7' and g == '|':
-            inc crossings
-          elif p == 'J' and g == '|':
-            inc crossings
-          elif p == 'L' and g == '7':
-            inc crossings
-          elif p == 'F' and (g == 'J' or g == 'S'):
-            inc crossings
+        # Check for "crossings" of the loop to figure out if we are inside or
+        # outside of it. ray scan?
+        if Pos(r: r, c: c) in loop:
+          if   prevPipe == '.' and currPipe == '|': inc crossings
+          elif prevPipe == '|' and currPipe == '|': inc crossings
+          elif prevPipe == '7' and currPipe == '|': inc crossings
+          elif prevPipe == 'J' and currPipe == '|': inc crossings
+          elif prevPipe == 'L' and currPipe == '7': inc crossings
+          elif prevPipe == 'L' and currPipe == 'S': inc crossings
+          elif prevPipe == 'F' and currPipe == 'J': inc crossings
+          elif prevPipe == 'F' and currPipe == 'S': inc crossings
 
-          if g != '-':
-            p = g
-          stdout.write fancy(g)
+          # Skip horizontal pipes since they don't help us figure out crossings.
+          if currPipe != '-': prevPipe = currPipe
+
+          cleanedGrid[r][c] = currPipe
         else:
           if crossings mod 2 == 1:
-            stdout.write "I"
+            cleanedGrid[r][c] = 'I'
             inc result
           else:
-            if g == '.':
-              stdout.write "O"
-            else:
-              stdout.write " "
+            cleanedGrid[r][c] = 'O'
 
+    # Print the cleaned up grid
+    for r in 0..cleanedGrid.high:
+      for c in 0..cleanedGrid[r].high:
+        stdout.write fancy cleanedGrid[r][c]
       echo ""
 
   verifyPart(1, 6907)
-  # 123 is too low
-  # 553 is too high
   verifyPart(2, 541)
