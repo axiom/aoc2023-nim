1cf6c28197824ec5f5faead77ae160cf05bf0307
Johannes Martinsson <johannes@antiklimax.se>
2023-12-13 23:31:48 +0100

    Finally solve part 2 of day 13

--- aoc202313.nim
+++ aoc202313.nim
@@ -1,39 +1,77 @@
 import aocd
-import std/[deques, strformat, strscans, strutils, sequtils, unittest]
+import std/[strformat, strutils, sequtils, unittest]
 
-proc reflectUpDown(grid: seq[string], foldAt: int): bool =
-  if foldAt <= 0 or foldAt >= grid.len:
-    return false
+type
+  Pos = object
+    y, x: int
 
+const BadPos = Pos(y: -1, x: -1)
+
+iterator findSmudges(grid: seq[string], foldBefore: int): Pos =
+  ## Find the location of all "smudges" i.e., positions that do not reflect
+  ## perfectly in their counterpart.
+
+  var errors = 0
+  let width = grid[0].len
   let height = grid.len
-  let halfHeight = height div 2
 
-  # echo fmt"reflectUpDown: foldAt={foldAt}, height={height}, halfHeight={halfHeight}"
+  for d in 1..grid.len:
+    let y1 = foldBefore - d
+    let y2 = foldBefore + d - 1
 
-  result = true
-  for distance in 1..halfHeight:
-    let y1 = foldAt - distance
-    let y2 = foldAt + distance - 1
-    # echo fmt"reflectUpDown: distance={distance}, y1={y1}, y2={y2}"
+    # Keep going until we fall off either end
     if y1 notin 0..<height or y2 notin 0..<height:
-      # echo fmt"reflectUpDown: out of bounds"
       break
 
-    # echo fmt"reflectUpDown: grid[y1]={grid[y1]}, grid[y2]={grid[y2]}"
-    if grid[y1] != grid[y2]:
-      # echo fmt"reflectUpDown: mismatch"
-      result = false
-      break
+    for x in 0..<width:
+      let c1 = grid[y1][x]
+      let c2 = grid[y2][x]
+      if c1 != c2 and c1 != '?' and c2 != '?':
+        yield Pos(y: y1, x: x)
+        inc errors
+
+iterator findFoldWithSmudges(grid: seq[string], count: int): (int, seq[Pos]) =
+  ## Find a folding position with exactly count smudges. The fold line and the
+  ## smudge positions are returned.
+  for fold in 1..grid.high:
+    var smudgeCount = 0
+    var smudges: seq[Pos]
+    for smudge in findSmudges(grid, fold):
+      smudges.add smudge
+      inc smudgeCount
+    if smudgeCount == count:
+      yield (fold, smudges)
+
+proc firstPerfectFold(grid: seq[string]; unless: int): int =
+  ## Find the first folding position, as long as it isnt unless, that reflects
+  ## perfectly, and return the number of lines above the fold.
+  result = -1
+  for (fold, _) in findFoldWithSmudges(grid, 0):
+    if result != unless:
+      return fold
+
+proc findFoldWithSingleSmudge(grid: seq[string], unless: int): (int, Pos) =
+  ## Find a single smudge in the grid, and return its position.
+  result = (-1, BadPos)
+  for (fold, smudges) in findFoldWithSmudges(grid, 1):
+    if fold != unless:
+      return (fold, smudges[0])
 
 func `*`(s: string, repeats: int): string =
   if repeats <= 0: return s
   return s & (s * (repeats - 1))
 
 proc transpose(grid: seq[string]): seq[string] =
-  result = newSeqWith[string](grid[0].len, "." * grid.len)
+  result = newSeqWith[string](grid[0].len, "O" * grid.high)
   for y in 0..grid.high:
-    for x in 0..grid[y].high:
+    for x in 0..grid[0].high:
       result[x][y] = grid[y][x]
+
+proc copy(grid: seq[string]): seq[string] =
+  result = newSeqWith[string](grid.len, "O" * grid[0].high)
+  for y in 0..grid.high:
+    for x in 0..grid[0].high:
+      result[y][x] = grid[y][x]
 
 proc `$`(grid: seq[string]): string =
   for y in 0..grid.high:
@@ -42,13 +80,14 @@ proc `$`(grid: seq[string]): string =
       result &= "\n"
 
 const testExample {.used.} = """
-##
-##
-##
+####
+####
+####
+####
 """.strip.splitLines
 
-check reflectUpDown(testExample, 2) == true
-check reflectUpDown(testExample.transpose, 1) == true
+check testExample.findFoldWithSmudges(1).toSeq.len == 0
+check testExample.findFoldWithSmudges(0).toSeq.len == 3
 
 const testExample2 {.used.} = """
 #.##..##.
@@ -56,12 +95,9 @@ const testExample2 {.used.} = """
 ##......#
 ##......#
 ..#.##.#.
-..##..##.
+#.##..##.
 #.#.##.#.
 """.strip.splitLines
-
-check reflectUpDown(testExample2, 4) == true
-check reflectUpDown(testExample2.transpose, 5) == true
 
 const example1 {.used.} = """
 #.##..##.
@@ -83,30 +119,30 @@ const example1 {.used.} = """
 
 day 13:
   let grids = input.strip.splitLines.groupByBlank
+  # keep track of folds to avoid.
+  var unlessV = newSeqWith[int](grids.len, int.low)
+  var unlessH = newSeqWith[int](grids.len, int.low)
 
   part 1:
     result = 0
-    for grid in grids:
-      # echo ""
-      # echo grid
-      var gridResult = 0
-
-      for foldAt in 0..grid.high:
-        if reflectUpDown(grid, foldAt):
-          gridResult += 100 * foldAt
-          break
-
-      let gridd = grid.transpose
-      for foldAt in 0..gridd.high:
-        if reflectUpDown(gridd, foldAt):
-          gridResult += foldAt
-          break
-
-      result += gridResult
+    for i, grid in grids.pairs:
+      if (let aboveFold = firstPerfectFold(grid, unlessV[i]); aboveFold > 0):
+        unlessV[i] = aboveFold
+        result += 100 * aboveFold
+      elif (let leftOfFold = firstPerfectFold(grid.transpose, unlessH[i]); leftOfFold > 0):
+        unlessH[i] = leftOfFold
+        result += leftOfFold
 
   part 2:
-    0
+    result = 0
+    for i, grid in grids.pairs:
+      # Every mirror has _exactly_ one smudge, so find it in either the original
+      # or transposed grid, then correct it and proceed with the calculations?
 
-  # 30619 is too low
+      if (let (fold, smudge) = findFoldWithSingleSmudge(grid, unlessV[i]); fold != -1):
+        result += 100 * fold
+      elif (let (fold, _) = findFoldWithSingleSmudge(grid.transpose, unlessH[i]); fold != -1):
+        result += fold
+
   verifyPart(1, 30705)
-  verifyPart(2, 0)
+  verifyPart(2, 44615)
